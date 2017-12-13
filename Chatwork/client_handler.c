9c74c86068a9c79ff0207d7ceaea173b6d3ae0c4
NhanNT <nhannt.bk@gmail.com>
2017-12-13 08:44:59 +0700

    Update client

---
+++ Chatwork/client_handler.c
@@ -0,0 +1,96 @@
+#include "client_handler.h"
+
+char *fail_signin(char *rest) {
+    return rest;
+}
+
+char *not_found(char *rest) {
+    return rest;
+}
+
+char *success_sent(char *rest) {
+    return rest;
+}
+
+char *user_exit(char *rest) {
+    return rest;
+}
+
+char *fail_login(char *rest) {
+    return rest;
+}
+
+user_array success_login(char *rest) {
+    char *token;
+    user_array result;
+    int count = 0;
+    while ((token = strtok_r(rest, "|", &rest))) {
+        strcpy(result.users[count].name, strtok_r(token, " ", &token));
+        result.users[count].state = atoi(token);
+        count++;
+    }
+    result.count = count;
+    return result;
+}
+
+user_array success_signin(char *rest) {
+    char *token;
+    user_array result;
+    int count = 0;
+    while ((token = strtok_r(rest, "|", &rest))) {
+        strcpy(result.users[count].name, strtok_r(token, " ", &token));
+        result.users[count].state = atoi(token);
+        count++;
+    }
+    result.count = count;
+    return result;
+}
+
+void change_user_state(user_array *array, char *name, int cur_state) {
+    int i;
+    for (i = 0; i < array->count; i++) {
+        if (!strcmp(array->users[i].name, name)) {
+            if (cur_state) {
+                array->users[i].state = 0;
+            } else {
+                array->users[i].state = 1;
+            }
+            return;
+        }
+    }
+}
+
+void add_user(user_array *array, char *name) {
+    strcpy(array->users[array->count].name, name);
+    array->users[array->count].state = 1;
+    array->count++;
+}
+
+void show_users(user_array array) {
+    int i;
+    for (i = 0; i < array.count; i++) {
+        printf("%s\t%d\n", array.users[i].name, array.users[i].state);
+    }
+}
+
+char *already_login(char *rest) {
+    return rest;
+}
+
+void show_history(chat_history history) {
+    printf("History with %s\n", history.name);
+    int i;
+    for (i = 0; i < history.count; i++) {
+        printf("Sender: %s\nTime: %s\nContent: %s\n", history.messages[i].sender,
+               history.messages[i].sent_time, history.messages[i].content);
+    }
+}
+
+char *get_user_name(char *resource) {
+    const char s[2] = " ";
+    char *token;
+    token = strtok(resource, s);
+    return token;
+}
+
+
---
+++ Chatwork/client_handler.h
@@ -0,0 +1,59 @@
+#ifndef C_HANDLER_H
+#define C_HANDLER_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define LOGIN_FAIL "430"
+#define LOGIN_BEFORE "440"
+#define LOGIN_SUCCESS "230"
+#define SIGNUP_FAIL "451"
+#define SIGNUP_SUCCESS "232"
+#define EXIT "231"
+#define SENT_SUCCESS "201"
+#define HISTORY "350"
+#define NOT_FOUND "404"
+#define RECEIVE_MESSAGE "731"
+
+#define SEND "SEND"
+#define RECEIVE "RECV"
+
+typedef struct {
+    char name[30];
+    int state;
+} user;
+
+typedef struct {
+    int count;
+    user users[100];
+} user_array;
+
+typedef struct {
+    char sender[30];
+    char content[250];
+    char sent_time[30];
+} message;
+
+typedef struct {
+    char name[30];
+    int count;
+    message messages[100];
+} chat_history;
+
+char *not_found(char *rest);
+char *success_sent(char *rest);
+char *user_exit(char *rest);
+char *fail_login(char *rest);
+user_array success_login(char *rest);
+char *fail_signin(char *rest);
+user_array success_signin(char *rest);
+char *already_login(char *rest);
+
+char *get_user_name(char *resource);
+
+void add_user(user_array *array, char *name);
+void change_user_state(user_array *array, char *name, int cur_state);
+void show_users(user_array array);
+void show_history(chat_history history);
+#endif
